2cccfe6ecb7f4a7d0b8c77b52b92daacb4f71a36
Anton Wilhelm <timaschew@gmail.com>
2015-12-07 14:25:31 +0100

    fix binary path to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # environment variables cannot be passed to a npm script, so use this shell script
-gh-pages --repo https://$GH_TOKEN@github.com/a2-team/musik-kunstschule.git -d build
+node_modules/.bin/gh-pages --repo https://$GH_TOKEN@github.com/a2-team/musik-kunstschule -d build
