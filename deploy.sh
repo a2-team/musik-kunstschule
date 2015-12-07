be08c4805d7f699094f5dc26d96f0fa3441aaf7c
Anton Wilhelm <timaschew@gmail.com>
2015-12-07 14:35:26 +0100

    set git config for travis

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 # environment variables cannot be passed to a npm script, so use this shell script
+git config --global user.email "Travis"
+git config --global user.name "noreply@travis-ci.org"
 node_modules/.bin/gh-pages --repo https://$GH_TOKEN@github.com/a2-team/musik-kunstschule -d build
