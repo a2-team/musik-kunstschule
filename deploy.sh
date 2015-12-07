45b1c3c3c098e64706b14006b4e2c434090210d5
Anton Wilhelm <timaschew@gmail.com>
2015-12-07 15:18:34 +0100

    use alternative for tree

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # environment variables cannot be passed to a npm script, so use this shell script
 git config --global user.email "Travis"
 git config --global user.name "noreply@travis-ci.org"
-tree build
+# `tree` is not available on travis
+du -a build/
 node_modules/.bin/gh-pages --repo https://x-oauth-basic:$GH_TOKEN@github.com/a2-team/musik-kunstschule -d build
